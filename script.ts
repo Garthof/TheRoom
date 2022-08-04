1e973df4855a992aee14f386be5e902f441d0397
Julián Lamas-Rodríguez <julian@lamasrodriguez.com>
2022-08-04 11:16:58 +0200

    Read and write commands from user input

--- script.ts
+++ script.ts
@@ -1,1 +1,21 @@
-console.log('Hello World :)');
+let story = <HTMLTextAreaElement>document.getElementById('story');
+let command = <HTMLInputElement>document.getElementById('command');
+
+if (story != null) {
+    story.innerHTML = 'Hello World! :)';
+}
+
+function appendTextToStory(text: string) {
+    if (story != null) {
+        story.innerHTML = story.innerHTML.concat('\n', text);
+    }
+}
+
+function commandEntered() {
+    console.log('Command Entered');
+    if (story != null && command != null) {
+        const commandText = '>'.concat(' ', command.value);
+        appendTextToStory(commandText);
+        console.log(commandText);
+    }
+}
